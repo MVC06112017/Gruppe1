f984a68ff96496957232df8c0f38601736c281d3
Ivan <Ivan@WINDOWS-J0BM7AR>
2017-11-24 14:56:27 +0100

    First model (classes)

---
+++ Gr1Photosharing/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gr1Photosharing.Models
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public int PhotoID { get; set; }
+        public string UserName { get; set; }
+        public string Subject { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Text { get; set; }
+        public virtual Photo Photo { get; set; }
+    }
+}
---
+++ Gr1Photosharing/Models/Photo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gr1Photosharing.Models
+{
+    public class Photo
+    {
+        public int PhotoID { get; set; }
+        public string Title { get; set; }
+
+        [DisplayName("Picture")]
+        public byte[] PhotoFile { get; set; }
+        public string MIMEType { get; set; }
+        public string Description { get; set; }
+
+        [DisplayName("Created Date")]
+        [DisplayFormat(DataFormatString = "{0: dd-MM-yy")]
+        public DateTime CreatedDate { get; set; }
+        public string UserName { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
